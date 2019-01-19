71770c0034f7758ec2eac34b16e3c4e8a7c17fa8
Daniel Adetayo <clevadani@gmail.com>
2019-01-19 17:03:32 +0100

    Made slight changes

--- server.js
+++ server.js
@@ -1,39 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
-const config = require('./config/serverConfig');
-mongoose.connect(`mongodb://localhost/${config.DB_NAME}`, 
- { useNewUrlParser: true }
-);
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const config = require("./config/serverConfig");
+mongoose.connect(`mongodb://localhost/${config.DB_NAME}`, {
+  useNewUrlParser: true
+});
 
-const itemApi = require('./routes/api/item');
+const itemApi = require("./routes/api/item");
 
 const PORT = process.env.PORT || config.PORT;
 
 app.use(bodyParser.json());
 
-app.use('/api/items', itemApi)
-
-app.get('*', (req, res) => {
-  res.send('This is a working route');
-})
+app.use("/api/items", itemApi);
 
 //Serve static asets if in production
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
-app.listen(PORT, (err) => {
+app.listen(PORT, err => {
   if (err) {
     console.log(`Server Error ${err}`);
   } else {
-    console.log(`Server running on port: ${PORT}`);   
+    console.log(`Server running on port: ${PORT}`);
   }
 });
